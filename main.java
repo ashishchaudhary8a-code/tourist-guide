5db2972ef226c737489bfd8612481e27c0fb4b64
Ashish Chaudhary <ashishchaudhary8a@gmail.com>
2026-03-03 00:16:15 +0530

    Initial commit of Tourist Helpline project

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.awt.Desktop;
+import java.io.File;
+
+public class main {
+    public static void main(String[] args) {
+        try {
+            File htmlFile = new File("ui/index.html");
+            Desktop.getDesktop().browse(htmlFile.toURI());
+        } catch (Exception e) {
+            System.out.println("Unable to open application");
+        }
+    }
+}
