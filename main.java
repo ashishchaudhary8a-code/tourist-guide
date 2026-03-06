59bf1988cf118b6195d8336c72e9a09d5f200eeb
Ashish Chaudhary <ashishchaudhary8a@gmail.com>
2026-03-06 17:10:57 +0530

    Initial project upload

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ import java.io.File;
 public class main {
     public static void main(String[] args) {
         try {
-            File htmlFile = new File("ui/index.html");
+            File htmlFile = new File("index.html");
             Desktop.getDesktop().browse(htmlFile.toURI());
         } catch (Exception e) {
             System.out.println("Unable to open application");
