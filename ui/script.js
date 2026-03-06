9771851582c4fd311ffba7da5385a27b78f864d9
Ashish Chaudhary <ashishchaudhary8a@gmail.com>
2026-03-06 17:42:35 +0530

    Fixed folder names to lowercase 'ui' and updated paths in HTML and JS

--- ui/script.js
+++ ui/script.js
@@ -6,54 +6,54 @@ document.getElementById("country").addEventListener("change", function () {
     if (country === "india") {
         box.innerHTML = `
             <h3>
-                <img src="UI/images/india.png" class="flag">
+                <img src="ui/images/india.png" class="flag">
                 India – Emergency Services
             </h3>
 
             <div class="card">
-                <img src="UI/images/police.png" class="icon">
+                <img src="ui/images/police.png" class="icon">
                 Police
                 <br><a href="tel:100">Call 100</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/ambulance.png" class="icon">
+                <img src="ui/images/ambulance.png" class="icon">
                 Ambulance
                 <br><a href="tel:108">Call 108</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/fire.png" class="icon">
+                <img src="ui/images/fire.png" class="icon">
                 Fire Brigade
                 <br><a href="tel:101">Call 101</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/women.png" class="icon">
+                <img src="ui/images/women.png" class="icon">
                 Women Safety
                 <br><a href="tel:181">Call 181</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/child.png" class="icon">
+                <img src="ui/images/child.png" class="icon">
                 Child Helpline
                 <br><a href="tel:1098">Call 1098</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/disaster.png" class="icon">
+                <img src="ui/images/disaster.png" class="icon">
                 Disaster / Emergency
                 <br><a href="tel:112">Call 112</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/tourist.png" class="icon">
+                <img src="ui/images/tourist.png" class="icon">
                 Tourist Helpline
                 <br><a href="tel:1363">Call 1363</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/senior.png" class="icon">
+                <img src="ui/images/senior.png" class="icon">
                 Senior Citizen Helpline
                 <br><a href="tel:14567">Call 14567</a>
             </div>
@@ -62,30 +62,30 @@ document.getElementById("country").addEventListener("change", function () {
     else if (country === "usa") {
         box.innerHTML = `
             <h3>
-                <img src="UI/images/usa.png" class="flag">
+                <img src="ui/images/usa.png" class="flag">
                 USA – Emergency Services
             </h3>
 
             <div class="card">
-                <img src="UI/images/police.png" class="icon">
+                <img src="ui/images/police.png" class="icon">
                 Emergency (Police / Fire / Medical)
                 <br><a href="tel:911">Call 911</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/women.png" class="icon">
+                <img src="ui/images/women.png" class="icon">
                 Women Safety
                 <br><a href="tel:8007997233">Call 800-799-7233</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/child.png" class="icon">
+                <img src="ui/images/child.png" class="icon">
                 Child Help
                 <br><a href="tel:18004224453">Call 1-800-422-4453</a>
             </div>
 
             <div class="card">
-                <img src="UI/images/tourist.png" class="icon">
+                <img src="ui/images/tourist.png" class="icon">
                 Tourist Information
                 <br><a href="tel:18004663236">Call 1-800-466-3236</a>
             </div>
